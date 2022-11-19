c62aa5c2bc178af7fa2fbea88fbc5fdba6fa88d7
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-19 22:00:53 +0200

    Remove redundant useEffect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,14 @@
-import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { useEffect } from "react";
-import { loginRequest } from "../services/user/login";
+import { AuthProvider, useAuth } from "../context/authContext";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    const asyncFunc = async () => {
-      const res = await loginRequest({
-        email: process.env.NEXT_PUBLIC_TEST_USER_EMAIL as string,
-        password: process.env.NEXT_PUBLIC_TEST_USER_PASSWORD as string,
-      });
-    };
-
-    asyncFunc();
-  }, []);
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   );
 }
