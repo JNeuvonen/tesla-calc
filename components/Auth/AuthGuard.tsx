f4776b8b7a76f0506a468e88243599df495d783f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 00:34:49 +0200

    Create AuthGuard.tsx

---
+++ components/Auth/AuthGuard.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { useAuth } from "../../context/authContext";
+import LoadingSpinner from "../Util/LoadingSpinner";
+type Props = {
+  children: React.ReactNode;
+};
+const AuthGuard = ({ children }: Props) => {
+  const user = useAuth();
+  const initialFetchDone = useAuth().initialFetchDone;
+  const router = useRouter();
+
+  useEffect(() => {
+    const URL = window.location.href;
+
+    if (initialFetchDone) {
+      if (!user.user) {
+        if (!URL.includes("/login")) {
+          router.push("/login");
+        }
+      }
+    }
+  }, [initialFetchDone]);
+
+  if (!initialFetchDone) {
+    return <LoadingSpinner />;
+  }
+
+  return <>{children}</>;
+};
+export default AuthGuard;
