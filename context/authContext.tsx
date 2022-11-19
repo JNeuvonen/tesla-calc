1949ee4e604b0d0e5386561011af5ec71e681fab
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 00:36:34 +0200

    Comment out automated login of test user

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -5,24 +5,38 @@ import { loginRequest } from "../services/user/login";
 type authContext = {
   user: User | null;
   login: ({ email, password }: { email: string; password: string }) => void;
+  initialFetchDone: boolean;
+  isAuthenticated: () => boolean;
 };
 
 const AuthContext = createContext<authContext>({} as authContext);
 
 export const AuthProvider = ({ children }: { children?: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
+  const [initialFetchDone, setInitialFetchDone] = useState(false);
 
   useEffect(() => {
     const asyncFunc = async () => {
-      const res = await loginRequest({
-        email: process.env.NEXT_PUBLIC_TEST_USER_EMAIL as string,
-        password: process.env.NEXT_PUBLIC_TEST_USER_PASSWORD as string,
-      });
-      setUser(res.user);
+      //const res = await loginRequest({
+      //  email: process.env.NEXT_PUBLIC_TEST_USER_EMAIL as string,
+      //  password: process.env.NEXT_PUBLIC_TEST_USER_PASSWORD as string,
+      //});
+      //if (res.user) {
+      //  setUser(res.user);
+      //}
+
+      setInitialFetchDone(true);
     };
 
     asyncFunc();
   }, []);
+
+  const isAuthenticated = () => {
+    if (user?.ID) {
+      return true;
+    }
+    return false;
+  };
 
   const login = async ({
     email,
@@ -41,6 +55,8 @@ export const AuthProvider = ({ children }: { children?: React.ReactNode }) => {
   const provider = {
     user,
     login,
+    initialFetchDone,
+    isAuthenticated,
   };
 
   return (
