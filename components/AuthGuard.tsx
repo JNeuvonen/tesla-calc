f5ce6124533841f159aa081940e94cee93e528f4
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 00:35:05 +0200

    Delete AuthGuard.tsx

--- components/AuthGuard.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-type Props = {
-  children: React.ReactNode;
-};
-const AuthGuard = ({ children }: Props) => {
-  return <div>{children}</div>;
-};
-export default AuthGuard;
