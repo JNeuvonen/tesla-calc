ca26253531960d64b1943bd6be146a7a62d0d415
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 00:34:38 +0200

    Create Chakra Buttons

---
+++ chakra/buttons.ts
@@ -0,0 +1,31 @@
+import { ChakraProps } from "@chakra-ui/react";
+import { BLUE_100, BLUE_100_HOVER, RED_100, RED_100_HOVER } from "./colors";
+
+export const baseStyles: ChakraProps = {
+  borderRadius: "50px",
+  minWidth: "max-content",
+  textTransform: "capitalize",
+};
+
+export const solidButton = () => {
+  const styles: ChakraProps = {
+    bg: RED_100,
+    color: "white",
+    _hover: {
+      bg: RED_100_HOVER,
+    },
+    ...baseStyles,
+  };
+  return styles;
+};
+
+export const secondaryButton = () => {
+  return {
+    bg: BLUE_100,
+    color: "white",
+    _hover: {
+      bg: BLUE_100_HOVER,
+    },
+    ...baseStyles,
+  };
+};
