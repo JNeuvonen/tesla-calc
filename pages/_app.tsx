8bf4b3a5b3ec32e4d6aa72f7851bf79375b51c3a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 11:46:55 +0200

    Add sidemenu provider to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../chakra/theme";
 import { AuthProvider } from "../context/auth";
+import { SidemenuProvider } from "../context/Sidemenu/sidemenu";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <SidemenuProvider>
+          <Component {...pageProps} />
+        </SidemenuProvider>
       </ChakraProvider>
     </AuthProvider>
   );
