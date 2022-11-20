ca7917d65264ce40eaa9314a72d9eff266fb3aa9
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 22:25:19 +0200

    Create signupRequest

--- services/user/login.ts
+++ services/user/login.ts
@@ -21,3 +21,19 @@ export const loginRequest = async ({ email, password }: loginReqTypes) => {
 
   return ret;
 };
+
+export const signupRequest = async ({ email, password }: loginReqTypes) => {
+  const URL = process.env.NEXT_PUBLIC_BACKEND_HOST + "user/signup";
+  const res = await fetch(URL, {
+    method: "POST",
+    body: JSON.stringify({
+      payload: { email, password },
+    }),
+    headers: requestHeaders,
+    credentials: "include",
+  });
+
+  const ret = await res.json();
+
+  return ret;
+};
