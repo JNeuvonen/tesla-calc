c8ec73c988f32adaca5c0e36b22d3cd237fb50f4
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-21 00:56:49 +0200

    Update login.ts

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -27,7 +27,7 @@ export default async function handler(
       });
 
       if (!user) {
-        res.status(400);
+        res.status(400).send({ message: "Incorrect credentials" });
         return;
       }
 
@@ -59,5 +59,5 @@ export default async function handler(
     }
   }
 
-  res.status(400).send({ message: "Bad Credentials" });
+  res.status(400).send({ message: "Incorrect Credentials" });
 }
