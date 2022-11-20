85af4140b1c2f1c9c8b6aa0e0b2533ace5d71ffc
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 10:12:00 +0200

    Create SideMenu.tsx

---
+++ components/Nav/SideMenu.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import useWindowDimensions from "../../utils/hooks/windowDimensions";
+const SideMenu = () => {
+  const { width } = useWindowDimensions();
+  return <div></div>;
+};
+export default SideMenu;
