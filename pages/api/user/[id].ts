291aacc8dc05dc18496009905a41c119e11d8118
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 22:34:10 +0200

    Remove build errors

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -2,23 +2,34 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { UserType } from "../../../types/prisma";
 import prisma from "../../../lib/prisma";
+import { hasToken } from "../../../lib/auth";
 
 type Data = {
-  user: UserType | null;
+  user?: UserType | null;
+  message?: string;
 };
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const token = hasToken(req);
+
+  if (!token) {
+    res.status(401).json({ message: "Unauthenticated" });
+    return;
+  }
+
   if (req.method === "GET") {
     try {
       const ID = Number(req.query.id);
+
       const users = await prisma.user.findFirst({
         where: {
           ID,
         },
       });
+
       res.status(200).json({ user: users });
     } catch (err) {
       res.status(400);
