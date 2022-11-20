1a1e2c3c957093750c5dece7cf2f0694b8fd663a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 10:12:58 +0200

    Update import path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../chakra/theme";
-import { AuthProvider } from "../context/authContext";
+import { AuthProvider } from "../context/auth";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
