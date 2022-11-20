734457af141b76b81c788bf6b6c34e4b748f516b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 16:30:28 +0200

    Add condition to layout rendering

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,17 +1,42 @@
+import { Box } from "@chakra-ui/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useAuth } from "../context/auth";
 import ContentContainer from "./ContentContainer";
 import SideMenu from "./Nav/Sidemenu/SideMenu";
 import TopNav from "./Nav/TopNav";
+import LoadingSpinner from "./Util/LoadingSpinner";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
+  const router = useRouter();
+  const path = router.asPath;
+  const initAuthFetchDone = useAuth().initialFetchDone;
+
+  if (!initAuthFetchDone) {
+    return (
+      <>
+        <Head>
+          <title>Page title</title>
+        </Head>
+        <LoadingSpinner />
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
         <title>Page title</title>
       </Head>
-      <TopNav />
-      <SideMenu />
-      <ContentContainer>{children}</ContentContainer>
+      {!path.includes("login") && !path.includes("signup") ? (
+        <>
+          <TopNav />
+          <SideMenu />
+          <ContentContainer>{children}</ContentContainer>
+        </>
+      ) : (
+        <Box>{children}</Box>
+      )}
     </>
   );
 }
