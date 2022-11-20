a544231fe7886c44a86656cd0a7e95bc3d30981a
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 13:39:02 +0200

    Update TopNav.tsx

--- components/Nav/TopNav.tsx
+++ components/Nav/TopNav.tsx
@@ -1,14 +1,30 @@
-import { Box, Button, Flex } from "@chakra-ui/react";
-import React from "react";
+import { Button, Flex } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { useAuth } from "../../context/auth";
 const TopNav = () => {
+  const router = useRouter();
+  const auth = useAuth();
   return (
-    <Flex justifyContent={"space-between"}>
-      <Box></Box>
-      <Box>
-        <Button variant={"secondary"} height={"35px"}>
-          Premium
-        </Button>
-      </Box>
+    <Flex
+      justifyContent={"space-between"}
+      flexDir={"row-reverse"}
+      position={"fixed"}
+      zIndex={2}
+      width={"100%"}
+      id={"header"}
+      height={"50px"}
+      top={0}
+      left={0}
+    >
+      <Button
+        variant={"primaryInverse"}
+        height={"30px"}
+        marginRight={"25px"}
+        marginTop={"10px"}
+        position={"relative"}
+      >
+        Go Premium
+      </Button>
     </Flex>
   );
 };
