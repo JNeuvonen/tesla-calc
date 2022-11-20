84d71d823370d0a61abc7d75dc293bc8202a4d69
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-20 13:38:48 +0200

    Use layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import theme from "../chakra/theme";
+import Layout from "../components/Layout";
 import { AuthProvider } from "../context/auth";
 import { SidemenuProvider } from "../context/Sidemenu/sidemenu";
 import "../styles/globals.css";
@@ -10,7 +11,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <AuthProvider>
       <ChakraProvider theme={theme}>
         <SidemenuProvider>
-          <Component {...pageProps} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
         </SidemenuProvider>
       </ChakraProvider>
     </AuthProvider>
