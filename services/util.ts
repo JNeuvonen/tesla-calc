a830ce382f40bf56e1837cad60f9555fd389fefe
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-25 17:42:55 +0200

    Create postRequest utility function

--- services/util.ts
+++ services/util.ts
@@ -1,15 +1,51 @@
 export const getRequest = async (endpoint: string) => {
-  const res = await fetch(endpoint);
-  return res;
+  try {
+    const res = await fetch(endpoint);
+
+    return res;
+  } catch (err) {
+    return null;
+  }
 };
 
 export const putRequest = (_endpoint: string) => {};
 
 export const delRequest = (_endpoint: string) => {};
 
-export const postRequest = (_endpoint: string) => {};
+export const postRequest = async ({
+  endpoint,
+  payload,
+}: {
+  endpoint: string;
+  payload?: Record<string | number, unknown>;
+}) => {
+  const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_HOST;
+
+  try {
+    const res = await fetch(
+      BASE_URL + endpoint,
+
+      {
+        method: "POST",
+        body: payload
+          ? JSON.stringify({
+              payload: payload,
+            })
+          : null,
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const parsedRes = await res.json();
+    return parsedRes;
+  } catch (err) {
+    return null;
+  }
+};
 
 export const requestHeaders = {
   "Content-Type": "application/json",
-  credentials: "include",
 };
