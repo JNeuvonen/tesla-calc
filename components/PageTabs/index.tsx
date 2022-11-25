ebc26507cb20be809aa554a1b9bc7795c13cef9b
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-25 21:12:22 +0200

    Create PageTabs component
    Helps with navigating tab items inside page

---
+++ components/PageTabs/index.tsx
@@ -0,0 +1,65 @@
+import { Box, Divider, Flex } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { GREY_100 } from "../../chakra/colors";
+import { generateNewHref, stringIncludes } from "../../utils/functions/general";
+
+export default function PageTabs({ headers }: { headers: string[] }) {
+  const router = useRouter();
+  const { asPath } = router;
+
+  return (
+    <ContentStyleWrapper>
+      <Flex columnGap={"16px"} rowGap={"16px"} flexWrap={"wrap"}>
+        {headers.map((item, i: number) => {
+          const activated = stringIncludes(asPath, item);
+          return (
+            <Link href={generateNewHref(asPath, item, 2)} key={i}>
+              <NavItemStyleWrapper activated={activated}>
+                {item}
+
+                {activated && (
+                  <Divider
+                    borderColor={"black"}
+                    borderWidth={"1px"}
+                    borderRadius={"50px"}
+                  />
+                )}
+              </NavItemStyleWrapper>
+            </Link>
+          );
+        })}
+      </Flex>
+    </ContentStyleWrapper>
+  );
+}
+
+const NavItemStyleWrapper = ({
+  children,
+  activated,
+}: {
+  children?: React.ReactNode;
+  activated: boolean;
+}) => {
+  return (
+    <Box fontWeight={activated ? "700" : "600"} cursor={"pointer"}>
+      {children}
+    </Box>
+  );
+};
+
+const ContentStyleWrapper = ({ children }: { children?: React.ReactNode }) => {
+  return (
+    <Box
+      width={"calc(100% + 48px)"}
+      bg={GREY_100}
+      left={"-24px"}
+      top={"-24px"}
+      height={"max-content"}
+      position="relative"
+      paddingBottom={"24px"}
+    >
+      {children}
+    </Box>
+  );
+};
