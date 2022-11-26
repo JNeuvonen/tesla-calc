02113bbe241646f64ec29b1f0471227b0c43a049
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-11-26 10:17:28 +0200

    Add GREY_200

--- chakra/colors.ts
+++ chakra/colors.ts
@@ -3,3 +3,4 @@ export const BLUE_100_HOVER = "#164d7a";
 export const RED_100 = "#D95B6F";
 export const RED_100_HOVER = "#a12539";
 export const GREY_100 = "#F1F3F5";
+export const GREY_200 = "#e8e8e4";
