28ee5cdc3ffcddbd989aaa7afb62100cc2b26912
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-01 19:37:16 +0200

    Add TEXT_BLACK_100 color constant

--- chakra/colors.ts
+++ chakra/colors.ts
@@ -4,3 +4,4 @@ export const RED_100 = "#D95B6F";
 export const RED_100_HOVER = "#a12539";
 export const GREY_100 = "#F1F3F5";
 export const GREY_200 = "#e8e8e4";
+export const TEXT_BLACK_100 = "#6c6c6c";
