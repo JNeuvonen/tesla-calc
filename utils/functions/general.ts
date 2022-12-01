e890a8598c81541dae9e4fd0101c55c42a2ea4d8
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-01 19:40:40 +0200

    Add utility functions

--- utils/functions/general.ts
+++ utils/functions/general.ts
@@ -1,9 +1,20 @@
+import { WeightCategory } from "../../components/Kuljetukset/createListing";
 import { getRequest } from "../../services/util";
 
-export const getInputFieldValById = (id: string) => {
+export const getInputFieldValById = (id: string, useDefaultvalue = false) => {
   const input = document.getElementById(id) as HTMLInputElement | null;
   if (!input) {
     return null;
+  }
+
+  if (useDefaultvalue) {
+    if (input.value) {
+      return input.value;
+    }
+
+    if (input.defaultValue) {
+      return input.defaultValue;
+    }
   }
 
   return input.value;
@@ -70,4 +81,45 @@ export const destructureLngLtFromGeoloc = (pos: GeolocationPosition | null) => {
     lng: pos.coords.longitude,
     lat: pos.coords.latitude,
   };
+};
+
+export const removeSpecialCharachters = (a: string) => {
+  return a.replace(/[^A-Z0-9]/gi, "");
+};
+
+export const stringIncludesDeepComparison = (a: string, b: string) => {
+  const formattedA = removeSpecialCharachters(a).toLowerCase();
+  const formattedB = removeSpecialCharachters(b).toLowerCase();
+
+  return formattedA.includes(formattedB) || formattedB.includes(formattedA);
+};
+
+export const addressComparison = (a: string, b: string) => {
+  try {
+    const aStreet = a.split(" ")[0];
+    const bStreet = b.split(" ")[0];
+
+    return stringIncludesDeepComparison(aStreet, bStreet);
+  } catch (err) {
+    return false;
+  }
+};
+
+export const weightCategoryFormatted = (weightCategory: WeightCategory) => {
+  if (weightCategory === "LIGHT") {
+    return "Alle 50 kg";
+  }
+
+  if (weightCategory === "MEDIUM") {
+    return "50-100 kg";
+  }
+
+  if (weightCategory === "HEAVY") {
+    return "100-300 kg";
+  }
+  if (weightCategory === "HEAVY-XL") {
+    return "Enemmän kuin 300 kg";
+  }
+
+  return "";
 };
