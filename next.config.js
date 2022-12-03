070508ccfcd9344a137e6eec522e46e596a1ce04
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-03 14:42:43 +0200

    Add AWS to allowed image urls in nextConfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,26 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "roudaaja.s3.amazonaws.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "roudaaja.s3.eu-north-1.amazonaws.com",
+        pathname: "/**",
+      },
 
-module.exports = nextConfig
+      {
+        protocol: "https",
+        hostname: "roudaaja.s3",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
