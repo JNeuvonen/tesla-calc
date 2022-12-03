1956061e6464ba7a080a83e96f86fb0a2162aa1f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-03 14:41:38 +0200

    Add props, spinner and main picture

--- components/Kuljetukset/CreateListing/SubmitModalContent.tsx
+++ components/Kuljetukset/CreateListing/SubmitModalContent.tsx
@@ -1,18 +1,27 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
-import { DriveDetails } from ".";
+import { DriveDetails } from "./types";
 import GreyText from "../../StyleWrappers/GreyText";
+import LoadingSpinner from "../../Util/LoadingSpinner";
+import ResponsiveImage from "../../Util/ResponsiveImage";
 
 type Props = {
   driveDetails: DriveDetails;
   origin: any;
   target: any;
+  mainpictureForForm: string;
+  sendData: () => void;
 };
 
 export default function SubmitModalContent({
   driveDetails,
   origin,
   target,
+  mainpictureForForm,
+  sendData,
 }: Props) {
+  if (!mainpictureForForm) {
+    return <LoadingSpinner />;
+  }
   return (
     <Box paddingBottom={"30px"}>
       <Box marginTop={"16px"}>
@@ -25,13 +34,20 @@ export default function SubmitModalContent({
         </Flex>
       </Box>
 
+      <Box marginTop={"16px"}>
+        <GreyText fontWeight={"bold"}>Ilmoituksen kuva</GreyText>
+        <Box marginTop={"6px"}>
+          <ResponsiveImage src={mainpictureForForm} />
+        </Box>
+      </Box>
+
       <Flex
         width={"100%"}
         flexDir={"column"}
         rowGap={"16px"}
         marginTop={"16px"}
       >
-        <Button width={"100%"} textTransform={"initial"}>
+        <Button width={"100%"} textTransform={"initial"} onClick={sendData}>
           Lähetä
         </Button>
         <Button
