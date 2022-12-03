554bc2c6ef0ea9faa41590cd94fa51799bfc80ba
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-03 14:44:54 +0200

    Update requests boilerplate

--- services/util.ts
+++ services/util.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 export const getRequest = async (
   endpoint: string,
   urlSearchParams?: URLSearchParams | null
@@ -15,7 +17,36 @@ export const getRequest = async (
   }
 };
 
-export const putRequest = (_endpoint: string) => {};
+export const putRequest = async (
+  endpoint: string,
+  payload?: Record<string | number, unknown>
+) => {
+  const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_HOST;
+
+  try {
+    const res = await fetch(
+      BASE_URL + endpoint,
+
+      {
+        method: "PUT",
+        body: payload
+          ? JSON.stringify({
+              payload: payload,
+            })
+          : null,
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const parsedRes = await res.json();
+    return parsedRes;
+  } catch (err) {
+    return null;
+  }
+};
 
 export const delRequest = (_endpoint: string) => {};
 
@@ -53,6 +84,34 @@ export const postRequest = async ({
   }
 };
 
+export const bulkUploadFiles = async (
+  attachments: FileList,
+  selectedMainAttachment: number
+) => {
+  const fileLocations = [];
+  let mainPicture = "";
+  for (let i = 0; i < attachments.length; i++) {
+    const formData = new FormData();
+    formData.append("file", attachments[i], attachments[i].name);
+    const res = await axios.post(
+      (process.env.NEXT_PUBLIC_BACKEND_HOST as string) + "aws/upload-file",
+      formData
+    );
+    const imagePath = res.data.message;
+    fileLocations.push(imagePath);
+
+    if (i === selectedMainAttachment) {
+      mainPicture = imagePath;
+    }
+  }
+
+  return { fileLocations, mainPicture };
+};
+
 export const requestHeaders = {
   "Content-Type": "application/json",
+};
+
+export const multipartFormHeaders = {
+  "Content-type": "multipart/form-data",
 };
