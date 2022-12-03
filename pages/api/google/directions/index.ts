32e5e33a1bc870029baa19d23896169cfabc7d99
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-03 14:43:59 +0200

    Remove redundant import

--- pages/api/google/directions/index.ts
+++ pages/api/google/directions/index.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getDirectionsEndpoint } from "../../../../services/google/endpoints";
 
 type Data = {
   data?: Response | null;
