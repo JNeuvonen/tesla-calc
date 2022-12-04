f9cc6a77c7ea9a570ce2410e76ca243858e8d17c
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-04 09:09:45 +0200

    Add landing page boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { Box, Button } from "@chakra-ui/react";
+import Link from "next/link";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <Box padding={"24px"}>
+      <Box>
+        <Link href={"/login"}>
+          <Button>Kirjaudu</Button>
+        </Link>
+      </Box>
+      Landing page TODO
+    </Box>
+  );
 }
