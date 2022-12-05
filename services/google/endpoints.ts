006af04f76b3a6034453300bd6b358e6cc6ff46f
Jarno Neuvonen <74303261+JNeuvonen@users.noreply.github.com>
2022-12-05 09:54:28 +0200

    Add getGeocodeEndpoint

--- services/google/endpoints.ts
+++ services/google/endpoints.ts
@@ -7,3 +7,7 @@ export const getDirectionsEndpoint = ({
 }) => {
   return `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}2&destination=${target}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
 };
+
+export const getGeocodeEndpoint = (address: string) => {
+  return `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`;
+};
